ccbf25cd618aa83391dffd518e8a8ffda5129533
Daniel Gurney <dgurney99@gmail.com>
2019-01-02 12:52:01 +0200

    Validation tests now cover everything
    The valid boolean allows preserving verbose output while also making testing possible.

--- validation/validate.go
+++ validation/validate.go
@@ -6,6 +6,9 @@ import (
 	"strconv"
 )
 
+// The boolean is only used for testing.
+var valid = true
+
 func checkdigitCheck(c string) bool {
 	// Check digit cannot be 0 or >= 8.
 	if c[len(c)-1:] == "0" || c[len(c)-1:] >= "8" {
@@ -24,30 +27,33 @@ func digitsum(num int64) int64 {
 	return s
 }
 
-// TODO: The rest of the file is entirely uncovered by testing.
-
 func validateCDKey(key string) error {
 	site, err := strconv.ParseInt(key[0:3], 10, 0)
 	if err != nil {
+		valid = false
 		return fmt.Errorf("the site number isn't a number")
 	}
 	main, err := strconv.ParseInt(key[4:11], 10, 0)
 	if err != nil {
+		valid = false
 		return fmt.Errorf("the second segment isn't a number")
 	}
 	invalidSites := map[int64]int{333: 333, 444: 444, 555: 555, 666: 666, 777: 777, 888: 888, 999: 999}
 	_, invalid := invalidSites[site]
 	if invalid {
+		valid = false
 		fmt.Println("The site number is invalid: cannot be 333, 444, 555, 666, 777, 888, or 999.")
 	}
 
 	c := strconv.Itoa(int(main))
 	secondSegmentInvalid := "The second segment is invalid:"
 	if !checkdigitCheck(c) {
+		valid = false
 		fmt.Println(secondSegmentInvalid, "the last digit cannot be 0 or >= 8.")
 	}
 	sum := digitsum(main)
 	if sum%7 != 0 {
+		valid = false
 		fmt.Printf("%s the digit sum (%d) must be divisible by 7.\n", secondSegmentInvalid, sum)
 	}
 	return nil
@@ -56,45 +62,54 @@ func validateCDKey(key string) error {
 func validateOEM(key string) error {
 	_, err := strconv.ParseInt(key[0:5], 10, 0)
 	if err != nil {
+		valid = false
 		return fmt.Errorf("the first segment is not a number")
 	}
 	th, err := strconv.ParseInt(key[10:17], 10, 0)
 	if err != nil {
+		valid = false
 		return fmt.Errorf("the third segment is not a number")
 	}
 	_, err = strconv.ParseInt(key[18:], 10, 0)
 	if err != nil {
+		valid = false
 		return fmt.Errorf("the fourth segment is not a number")
 	}
 	julian, err := strconv.ParseInt(key[0:3], 10, 0)
 	if julian == 0 || julian > 366 {
+		valid = false
 		fmt.Println("The date is invalid: valid date range 001-366.")
 	}
 	year := key[3:5]
 	validYears := map[string]string{"95": "95", "96": "96", "97": "97", "98": "98", "99": "99", "00": "00", "01": "01", "02": "02", "03": "03"}
-	_, valid := validYears[year]
-	if !valid {
+	_, validYear := validYears[year]
+	if !validYear {
+		valid = false
 		fmt.Println("The year is invalid: cannot be less than 95 or above 03")
 	}
 
 	third := key[10:17]
 	thirdSegmentInvalid := "The third segment is invalid:"
 	if string(third[0]) != "0" {
+		valid = false
 		fmt.Println(thirdSegmentInvalid, "must begin with a 0.")
 	}
 	c := strconv.Itoa(int(th))
 	if !checkdigitCheck(c) {
+		valid = false
 		fmt.Println(thirdSegmentInvalid, "last digit cannot be 0 or >= 8.")
 	}
 	sum := digitsum(th)
 	if sum%7 != 0 {
+		valid = false
 		fmt.Printf("%s digit sum (%d) must be divisible by 7.\n", thirdSegmentInvalid, sum)
 	}
 	return nil
 }
 
 // ValidateKey validates the provided OEM or CD key.
-func ValidateKey(k string) {
+func ValidateKey(k string) bool {
+	valid = true
 	maybeValidMessage := "%s is valid if you get no further output.\n"
 	unableToValidate := "Unable to validate key:"
 	// Make sure the provided key has a chance of being valid.
@@ -102,14 +117,18 @@ func ValidateKey(k string) {
 	case len(k) == 11 && k[3:4] == "-":
 		fmt.Printf(maybeValidMessage, k)
 		if err := validateCDKey(k); err != nil {
+			valid = false
 			fmt.Println(unableToValidate, err)
 		}
 	case len(k) == 23 && k[5:6] == "-" && k[9:10] == "-" && k[17:18] == "-" && len(k[18:]) == 5:
 		fmt.Printf(maybeValidMessage, k)
 		if err := validateOEM(k); err != nil {
+			valid = false
 			fmt.Println(unableToValidate, err)
 		}
 	default:
+		valid = false
 		fmt.Printf("%s doesn't even resemble a valid key.\n", k)
 	}
+	return valid
 }
--- validation/validate_test.go
+++ validation/validate_test.go
@@ -6,52 +6,74 @@ import (
 	"testing"
 )
 
+var validKeys = []struct {
+	key string
+}{
+	{"111-1111111"},
+	{"000-0000007"},
+	{"10000-OEM-0000007-00000"},
+	{"32299-OEM-0840621-16752"},
+	{"118-5688143"},
+}
+var invalidKeys = []struct {
+	key string
+}{
+	// Not even close to a valid key
+	{"1"},
+	{"10000-OEM-0000007-1"},
+	// Invalid date
+	{"00099-OEM-0840621-16752"},
+	{"36799-OEM-0840621-16752"},
+	// Invalid year
+	{"10094-OEM-0840621-16752"},
+	{"10019-OEM-0840621-16752"},
+	// Invalid site
+	{"333-5688143"},
+	{"444-5688143"},
+	{"555-5688143"},
+	{"666-5688143"},
+	{"777-5688143"},
+	{"888-5688143"},
+	{"999-5688143"},
+	// Invalid check digit
+	{"10000-OEM-0140628-12345"},
+	{"332-5683148"},
+	// Invalid third segment starting digit
+	{"10000-OEM-8040621-12345"},
+	// Invalid digit sum
+	{"10000-OEM-0000006-12345"},
+	{"001-1234566"},
+	// Not a number
+	{"11a-1111111"},
+	{"111-a111111"},
+	{"1000a-OEM-0000007-11111"},
+	{"10000-OEM-00000a7-11111"},
+	{"10000-OEM-0000007-1111a"},
+}
+
+func TestSingleValidation(t *testing.T) {
+	for _, kt := range validKeys {
+		v := ValidateKey(kt.key)
+		switch {
+		default:
+			t.Logf("%s is valid, as expected.", kt.key)
+		case !v:
+			t.Errorf("Valid key %s did not pass validation!", kt.key)
+		}
+	}
+	for _, kt := range invalidKeys {
+		v := ValidateKey(kt.key)
+		switch {
+		default:
+			t.Logf("%s is not valid, as expected.", kt.key)
+		case v:
+			t.Errorf("Invalid key %s passed validation!", kt.key)
+		}
+	}
+}
+
 func TestBatchValidation(t *testing.T) {
 	vch := make(chan bool)
-	validKeys := []struct {
-		key string
-	}{
-		{"111-1111111"},
-		{"000-0000007"},
-		{"10000-OEM-0000007-00000"},
-		{"32299-OEM-0840621-16752"},
-		{"118-5688143"},
-	}
-	invalidKeys := []struct {
-		key string
-	}{
-		// Not even close to a valid key
-		{"1"},
-		{"10000-OEM-0000007-1"},
-		// Invalid date
-		{"00099-OEM-0840621-16752"},
-		{"36799-OEM-0840621-16752"},
-		// Invalid year
-		{"10094-OEM-0840621-16752"},
-		{"10019-OEM-0840621-16752"},
-		// Invalid site
-		{"333-5688143"},
-		{"444-5688143"},
-		{"555-5688143"},
-		{"666-5688143"},
-		{"777-5688143"},
-		{"888-5688143"},
-		{"999-5688143"},
-		// Invalid check digit
-		{"10000-OEM-0140628-12345"},
-		{"332-5683148"},
-		// Invalid third segment starting digit
-		{"10000-OEM-8040621-12345"},
-		// Invalid digit sum
-		{"10000-OEM-0000006-12345"},
-		{"001-1234566"},
-		// Not a number
-		{"11a-1111111"},
-		{"111-a111111"},
-		{"1000a-OEM-0000007-11111"},
-		{"10000-OEM-00000a7-11111"},
-		{"10000-OEM-0000007-1111a"},
-	}
 	for _, kt := range validKeys {
 		go BatchValidate(kt.key, vch)
 		switch {
